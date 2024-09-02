e0cb6be8a96ce7fd0b74d27bf2161a283d69e340
leftsidebrain <ruddikhai10@gmail.com>
2024-09-02 14:09:36 +0700

    u

--- src/hooks/api/index.ts
+++ src/hooks/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_URL || "https://deploy-be-plum.vercel.app/",
 });
 
 export const setAuthToken = (token?: string) => {
