a2158fdd4853453776aa0d5dfd12597d24f76680
leftsidebrain <ruddikhai10@gmail.com>
2024-09-03 10:56:11 +0700

    u

--- src/components/common/modal/ProfileModal.tsx
+++ src/components/common/modal/ProfileModal.tsx
@@ -152,12 +152,7 @@ export default function ProfileModal() {
             </form>
           </Box>
           <Box sx={{ mt: 2, borderTop: "1px solid white", padding: 2, display: "flex", justifyContent: "end" }}>
-            <Button
-              type="submit"
-              onClick={() => {
-                handleSubmit;
-              }}
-            >
+            <Button type="submit" onClick={handleSubmit}>
               Save
             </Button>
           </Box>
